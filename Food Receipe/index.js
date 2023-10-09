cfa8b71ac8f9544d3f694207511aa4e8bc647644
andreimaier <113696878+andreimaier@users.noreply.github.com>
2023-10-09 20:43:29 +0800

    modal functionality
    basic functionality for mock-up modal

---
+++ Food Receipe/index.js
@@ -0,0 +1,13 @@
+const getRecipeButtons = document.querySelectorAll('.result__get-recipe')
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.modal__close')
+
+getRecipeButtons.forEach(button => button.addEventListener('click', openModal))
+modalClose.addEventListener('click', closeModal)
+
+function openModal() {
+    modal.showModal()
+}
+function closeModal() {
+    modal.close()
+}
