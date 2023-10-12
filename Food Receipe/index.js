5e7a4ffbc29845f18260305ed7a1d4bca88639d6
Shree V <109030986+Shree-77@users.noreply.github.com>
2023-10-12 13:47:55 +0530

    Dialog functions added

--- Food Receipe/index.js
+++ Food Receipe/index.js
@@ -120,3 +120,19 @@ function clearResults() {
     recipeContainer.removeChild(recipeContainer.firstChild);
   }
 }
+
+//Dialog Box 
+
+const getRecipeButtons = document.querySelectorAll('.result__get-recipe')
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.modal__close')
+
+getRecipeButtons.forEach(button => button.addEventListener('click', openModal))
+modalClose.addEventListener('click', closeModal)
+
+function openModal() {
+    modal.showModal()
+}
+function closeModal() {
+    modal.close()
+}
